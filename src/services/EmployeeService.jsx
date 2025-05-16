c42bed025205834e85f5fd0dc18204247e617a45
RAKOTOARIVONY Herinjaka Antonio <rkt.herinjaka@gmail.com>
2025-05-16 17:25:27 +0200

    Fix bug url (#7)
    Co-authored-by: Herinjaka-Antonio RAKOTOARIVONY <herinjaka.rakotoarivony@gmail.com.com>

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -2,18 +2,18 @@ import api from './Api.jsx';
 
 export const EmployeeService = {
   getAllEmployees: async () => {
-    return api.get('/departments');
+    return api.get('/employees');
   },
 
   createEmployee: async (employee) => {
-    return api.post('/departments', employee);
+    return api.post('/employees', employee);
   },
 
   updateEmployee: async (id, employee) => {
-    return api.put(`/departments/${id}`, employee);
+    return api.put(`/employees/${id}`, employee);
   },
 
   deleteEmployee: async (id) => {
-    return api.delete(`/departments/${id}`);
+    return api.delete(`/employees/${id}`);
   },
 };
