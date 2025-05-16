bbe993559156ae8fb74e5a22d16b0b2e5cea2dd7
Herinjaka-Antonio RAKOTOARIVONY <herinjaka.rakotoarivony@gmail.com.com>
2025-05-16 17:20:47 +0200

    Fix bug url

--- src/services/SalaryService.jsx
+++ src/services/SalaryService.jsx
@@ -2,18 +2,18 @@ import api from './Api.jsx';
 
 export const SalaryService = {
   getAllSalary: async () => {
-    return api.get('/payrolls');
+    return api.get('/salaries');
   },
 
   createSalary: async (salary) => {
-    return api.post('/payrolls', salary);
+    return api.post('/salaries', salary);
   },
 
   updateSalary: async (id, salary) => {
-    return api.put(`/payrolls/${id}`, salary);
+    return api.put(`/salaries/${id}`, salary);
   },
 
   deleteSalary: async (id) => {
-    return api.delete(`/payrolls/${id}`);
+    return api.delete(`/salaries/${id}`);
   },
 };
