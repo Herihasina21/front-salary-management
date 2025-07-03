c35a1f1e40639a082b13c19cf1e11e9098658723
Herihasina21 <herihasinamichael@gmail.com>
2025-07-03 12:49:50 +0300

    change preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
 
 const PreLoader = () => {
-    return (
-      <div className="loader-bg">
-        <div className="loader-track">
-          <div className="loader-fill"></div>
-        </div>
-      </div>
-    );
-  };
-  
-  export default PreLoader;
+  return (
+    <div
+      className="loader-bg"
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#fff',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 9999,
+      }}
+    >
+      <img
+        src="/assets/images/logo.png"
+        alt="Chargement..."
+        style={{ width: '150px', height: 'auto' }}
+      />
+    </div>
+  );
+};
+
+export default PreLoader;
